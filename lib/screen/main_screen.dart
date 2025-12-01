24ce9991bc3bd507acab8c3a6c86cad58c9c8616
nazifanishat09 <nazifanishat2024@gmail.com>
2025-12-01 15:36:32 +0600

    2nd commit

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -25,23 +25,43 @@ class _MainScreenState extends State<MainScreen> {
       body: Column(
         children: [
           SearchBer(),
-          SizedBox(height: 20,),
-          ListView.builder(
-            itemCount: 20,
-            itemBuilder: (context, index) => Container(
-              height: 100,
-              width: 400,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey,
-                    blurRadius: 1,
-                    offset: Offset(0, 2),
-                    spreadRadius: 1,
+          SizedBox(height: 20),
+          Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+
+              itemCount: 20,
+              itemBuilder: (context, index) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  width: 400,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 1,
+                        offset: Offset(0, 2),
+                        spreadRadius: 1,
+                      ),
+                    ],
                   ),
-                ],
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(color: Colors.blue),
+                          child: Image(image: AssetImage("assets/qq.png",)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           ),
