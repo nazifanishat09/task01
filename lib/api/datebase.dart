0ab123bd3681a1e74e50f5197021aa1797d20771
nazifanishat09 <nazifanishat2024@gmail.com>
2025-12-01 18:46:04 +0600

    4th

---
+++ lib/api/datebase.dart
@@ -0,0 +1,84 @@
+class QuotData{
+  static List data =  [
+  {
+  "id": 193,
+  "category_id": "8",
+  "quote": "ধন অর্জন করতে চাও? প্রথমে জ্ঞান অর্জন করো।",
+  "author": "Benjamin Franklin",
+  "created_at": null,
+  "updated_at": null
+  },
+  {
+  "id": 34,
+  "category_id": "1",
+  "quote": "নিজের প্রতি আস্থা রাখো, পৃথিবী তোমার দিকে ঝুঁকবে।",
+  "author": "Unknown",
+  "created_at": null,
+  "updated_at": null
+  },
+  {
+  "id": 99,
+  "category_id": "4",
+  "quote": "প্রত্যেক ব্যর্থতার পরে নতুনভাবে শুরু করো।",
+  "author": "Unknown",
+  "created_at": null,
+  "updated_at": null
+  },
+  {
+  "id": 5,
+  "category_id": "9",
+  "quote": "হারানো সময় আর ফিরে আসে না।",
+  "author": "Benjamin Franklin",
+  "created_at": null,
+  "updated_at": null
+  },
+  {
+  "id": 42,
+  "category_id": "1",
+  "quote": "নিজেকে ভালোবাসা এবং বিশ্বাস করা, সাফল্যের চাবিকাঠি।",
+  "author": "Unknown",
+  "created_at": null,
+  "updated_at": null
+  },
+  {
+  "id": 8,
+  "category_id": "9",
+  "quote": "যতক্ষণ আমরা সময়কে নিয়ন্ত্রণ করতে পারি না, ততক্ষণ আমরা আর কিছু নিয়ন্ত্রণ করতে পারব না।",
+  "author": "Peter Drucker",
+  "created_at": null,
+  "updated_at": null
+  },
+  {
+  "id": 36,
+  "category_id": "1",
+  "quote": "নিজের প্রতি বিশ্বাসই প্রথম ধাপ সাফল্যের পথে।",
+  "author": "Anonymous",
+  "created_at": null,
+  "updated_at": null
+  },
+  {
+  "id": 160,
+  "category_id": "6",
+  "quote": "নিয়মিত অধ্যয়নই জীবনে পার্থক্য তৈরি করে।",
+  "author": "Malala Yousafzai",
+  "created_at": null,
+  "updated_at": null
+  },
+  {
+  "id": 63,
+  "category_id": "2",
+  "quote": "সফলতা অর্জনের জন্য প্রত্যেক দিন নতুন কিছু শিখতে হবে।",
+  "author": "Unknown",
+  "created_at": null,
+  "updated_at": null
+  },
+  {
+  "id": 153,
+  "category_id": "6",
+  "quote": "পড়াশোনা হলো ভবিষ্যতের সোনার চাবি।",
+  "author": "Nelson Mandela",
+  "created_at": null,
+  "updated_at": null
+  }
+  ];
+}
